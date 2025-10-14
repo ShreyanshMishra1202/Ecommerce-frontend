f4b2eb22ad7898222a007113de36c1fb87ef1771
Deepika Srivastava <dpka9509@gmain.com>
2025-10-14 14:28:40 +0530

    [Deepika]+added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,39 @@
 import React, { useState } from 'react'
-import Main from './pages/Main'
 import { Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
 import ProductDetails from './components/ProductDetails'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import CartPage from './pages/CartPage'
 import PageNotFound from './components/PageNotFound'
-
+import SignUpPage from './pages/SignUpPage'
 
 const App = () => { 
-    const savedDataString = localStorage.getItem("my-cart") || "{}";
-    const savedData = JSON.parse(savedDataString);
-    const [cart,setCart] = useState(savedData);
-    const hanldeAddToCart = function(productId,count){
-    count=+count;
-    let oldCount = cart[productId] || 0;
-    const newCart = {...cart, [productId]:oldCount+count};
-    updateCart(newCart);
-  };
+  const savedCart = JSON.parse(localStorage.getItem("my-cart") || "{}")
+  const [cart, setCart] = useState(savedCart)
 
-  function updateCart(newCart){
-    setCart(newCart);
-    localStorage.setItem("my-cart", JSON.stringify(newCart));
+  const handleAddToCart = (productId, count) => {
+    count = +count
+    const newCart = { ...cart, [productId]: (cart[productId] || 0) + count }
+    setCart(newCart)
+    localStorage.setItem("my-cart", JSON.stringify(newCart))
   }
 
-
-
-  const totalCount= Object.keys(cart).reduce(function (output,current){
-    return output+cart[current];
-  },0);
-  console.log(cart);
-
-
+  const totalCount = Object.values(cart).reduce((sum, qty) => sum + qty, 0)
 
   return (
-    <div>
-    <>
-    <Header productCount={totalCount} />
-    <div className='grow'>
-      <Routes>
-      <Route path='/' element={<Main addToCart={hanldeAddToCart} />}/>
-      <Route path='/details/:id' element={<ProductDetails addToCart={hanldeAddToCart} />}/>
-      <Route path='/cart' element={<CartPage setCart={setCart} updateCart={updateCart} data={cart}/>}/>
-      <Route path="/notfound" element={<PageNotFound/>}/>
-      </Routes>
-    </div>
-    <Footer/>
-    </>
-    
+    <div className="min-h-screen flex flex-col">
+      <Header productCount={totalCount} />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Main addToCart={handleAddToCart} />} />
+          <Route path="/details/:id" element={<ProductDetails addToCart={handleAddToCart} />} />
+          <Route path="/cart" element={<CartPage setCart={setCart} updateCart={setCart} data={cart} />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   )
 }
